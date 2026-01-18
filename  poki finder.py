6f737fdc20a8dfa06d8f6dc8f865a2b960593bae
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2026-01-18 12:00:40 +0300

    I have moved a little further.

---  poki finder.py
+++  poki finder.py
@@ -3,9 +3,21 @@ import json
 
 
 
-def make_request():
-    response = requests.get(url="https://pokeapi.co/api/v2/pokemon/ditto")
+def make_request(url):
+    response = requests.get(url)
     return response.json()
 
+print(make_request("https://pokeapi.co/api/v2/pokemon/ditto"))
 
-print(make_request())
+
+poki_response = make_request("https://pokeapi.co/api/v2/pokemon/ditto")
+
+#user_input = input("enter poki class: ")
+for poki in poki_response:
+    if poki == "game_indices":
+        for index in poki_response["game_indices"]:
+            print(index)
+
+
+
+
