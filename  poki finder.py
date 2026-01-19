b5106730746cf7303f87f61cd476c5aa908ed952
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2026-01-19 21:11:19 +0300

    Interface and background completed.

---  poki finder.py
+++  poki finder.py
@@ -3,16 +3,7 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("Poki Finder")
-window.geometry("900x700")
-
-
-
-foto_label = tkinter.Label(window, image=)
-foto_label.pack()
-
-
-
-
+window.geometry("500x600")
 
 
 
@@ -21,28 +12,33 @@ foto_label.pack()
 
 POKE_NAMES = ""
 
+
 def make_request(url):
     response = requests.get(url)
     return response.json()
 
-#print(make_request("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"))
+make_request("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+
+entry_1 = tkinter.Entry(window,font=("Arial", 14))
+entry_1.pack(pady=10)
+entry_1.pack()
 
 
-user_input = 'snorlax' #input("enter poki class: ")
+
 
 
 
 def find_names_urls():
+    user_input = entry_1.get()
     global POKE_NAMES
     poki_response = make_request("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
     for poki in poki_response:
         if poki == "results":
             for index in poki_response["results"]:
-                if index["name"] == user_input:
+                if index["name"] == user_input.lower():
                     POKE_NAMES =  index["url"]
-    return
+    return POKE_NAMES
 
-#print(find_names_urls())
 
 def poki_ab():
     empy_list = []
@@ -55,15 +51,34 @@ def poki_ab():
 
     return empy_list
 
-def poki_foto():
-    poke_names_response = make_request(POKE_NAMES)
-    return (poke_names_response['sprites']['front_default'])
+def functions():
+    try:
+        find_names_urls()
+        poki_ab()
+        label_2.config(text=f'Abilities : {poki_ab()}', fg="blue", font=("Arial", 15, "bold"))
+    except:
+        label_2.config(text='No Pokemon Found', fg="red")
 
 
 
-#print(poki_ab())
-#print(poki_foto())
 
+
+
+
+
+
+or_image = tkinter.PhotoImage(file='snorlax.png')
+resized_or_image = or_image.zoom(4,4)
+foto_label = tkinter.Label(window, image=resized_or_image, compound="top")
+foto_label.pack(pady=10)
+foto_label.pack()
+
+
+button = tkinter.Button(window, text="Find Name", command=functions, font=("Arial", 12))
+button.pack()
+
+label_2 = tkinter.Label(window, text='')
+label_2.pack()
 
 
 
